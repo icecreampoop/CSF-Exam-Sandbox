95997d77b17054e844ba3d0d8493cc4f2bba2354
icecreampoop <pchiku@hotmail.sg>
2024-06-12 17:03:04 +0800

    added all class code

---
+++ PlayingWithAngular/src/app/components/second-try/image-model.ts
@@ -0,0 +1,5 @@
+export interface ImageModel {
+    counter: number
+    caption: string
+    imageUrl: string
+}
---
+++ PlayingWithAngular/src/app/components/third-try/third-try.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-try',
+  templateUrl: './third-try.component.html',
+  styleUrl: './third-try.component.css'
+})
+export class ThirdTryComponent {
+
+}
